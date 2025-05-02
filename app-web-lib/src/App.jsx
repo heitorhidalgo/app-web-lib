4aa6f01b3c9741c2b5ecc7cd081242e1ff7125fc
Heitor Hidalgo <heitorhidalgo10@hotmail.com>
2025-05-02 19:36:37 -0300

    Criação do formulário de cadastro.

--- app-web-lib/src/App.jsx
+++ app-web-lib/src/App.jsx
@@ -4,16 +4,29 @@ import Container from "./components/layouts/Container";
 import Home from "./components/pages/home";
 import Createbooks from "./components/pages/CreateBook";
 import ListBooks from "./components/pages/ListBooks";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Container>
+      {/* <Container>
         <NavBar />
         <Home />
         <Createbooks />
         <ListBooks />
-      </Container>
+      </Container> */}
+
+      <BrowserRouter>
+        <Container>
+            <Routes>
+              <Route path='/' element={<NavBar />}>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/listBook' element={<ListBooks />} />
+                  <Route path='/newBook' element={<Createbooks />} />
+              </Route>
+            </Routes>
+        </Container>
+      </BrowserRouter>
     </>
   );
 }
